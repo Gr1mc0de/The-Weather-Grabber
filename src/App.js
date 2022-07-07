c1a9d84f63a5b28868de751586280e6b4a5baaa9
𓃫𓄤 <GR1MC0DE@protonmail.com>
2022-07-07 10:07:36 +0100

    v1.1 WIP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import Footer from './components/Footer/Footer';
 import Logo from './i/weather-grabber.png';
 
 export default function App() {
   return (
     <div>
       <Header Logo={Logo}/>
+      <Main/>
+      <Footer Logo={Logo}/>
     </div>
   );
 }
